025d1a9441884fd063f84dc20feb3bd3fd9b7ad2
Ali Samadi <alisamadi.096@gmail.com>
2025-03-24 23:41:20 -0500

    compelete Field.jsx based on solution Repo

--- src/Field.jsx
+++ src/Field.jsx
@@ -1,2 +1,16 @@
 import { useGame } from "./GameContext";
-export default function Field() {}
+export default function Field() {
+  const { field } = useGame();
+  return (
+    <ul>
+      {field.map((hasMole, i) => (
+        <Hole key={i} hasMole={hasMole} />
+      ))}
+    </ul>
+  );
+}
+function Hole({ hasMole }) {
+  const { Whack } = useGame();
+  if (hasMole) return <li onClick={Whack} className="hole mole"></li>;
+  return <li className="hole"></li>;
+}
