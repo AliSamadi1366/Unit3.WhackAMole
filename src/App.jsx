2da41ae72c3ad0fc000967a580b8b7dea71f4833
Ali Samadi <alisamadi.096@gmail.com>
2025-03-24 22:54:32 -0500

    compelete App.jsx with the solution repo as my refrence

--- src/App.jsx
+++ src/App.jsx
@@ -4,5 +4,18 @@ import Scoreboard from "./Scoreboard";
 import Field from "./Field";
 
 export default function App() {
-  
+  const { playing } = useGame();
+  return (
+    <>
+      <p>Whack a Mole!</p>
+      {playing ? (
+        <>
+          <Field />
+          <Scoreboard />
+        </>
+      ) : (
+        <Welcome />
+      )}
+    </>
+  );
 }
