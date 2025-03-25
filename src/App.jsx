4de21bce9c3d9e8affcc5f58ba62b723051b0db6
Ali Samadi <alisamadi.096@gmail.com>
2025-03-24 22:40:31 -0500

    initialize App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import { useGame } from "./GameContext";
+import Welcome from "./Welcome";
+import Scoreboard from "./Scoreboard";
+import Field from "./Field";
+
 export default function App() {
-  return <></>;
+  
 }
