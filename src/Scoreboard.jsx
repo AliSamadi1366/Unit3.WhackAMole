d853dc1f5e43c870f476b44177cb966697e18ace
Ali Samadi <alisamadi.096@gmail.com>
2025-05-30 09:31:27 -0500

    score and stop button were created

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,5 +1,10 @@
 import { useGame } from "./GameContext";
 export default function Scoreboard() {
- 
+  const { score, stop } = useGame();
+  return (
+    <div className="scoreboard">
+      <p>Score: {score}</p>
+      <button onClick={stop}>Restart</button>
+    </div>
   );
 }
